ca04a6ec8fb7635dd8faab13755ee53c7f0c80aa
Emerson Lima <emersonlimago123@gmail.com>
2024-07-22 14:07:43 -0300

    Added UserDTO

--- src/main/java/emerson/springbootapi/resources/UserResource.java
+++ src/main/java/emerson/springbootapi/resources/UserResource.java
@@ -1,8 +1,7 @@
 package emerson.springbootapi.resources;
 
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -11,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import emerson.springbootapi.domain.User;
+import emerson.springbootapi.dto.UserDTO;
 import emerson.springbootapi.services.UserService;
 
 @RestController
@@ -21,8 +21,9 @@ public class UserResource {
 	private UserService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
+	public ResponseEntity<List<UserDTO>> findAll(){
 		List<User> list = service.findAll();
-		return ResponseEntity.ok().body(list);
+		List<UserDTO> listDto = list.stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDto);
 	}
 }
