8236f6180cf223bb851ad0300ffe3c6a0b14fcb9
Emerson Lima <emersonlimago123@gmail.com>
2024-07-22 14:28:20 -0300

    Added finfById operation

--- src/main/java/emerson/springbootapi/services/UserService.java
+++ src/main/java/emerson/springbootapi/services/UserService.java
@@ -1,12 +1,14 @@
 package emerson.springbootapi.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import emerson.springbootapi.domain.User;
 import emerson.springbootapi.repository.UserRepository;
+import emerson.springbootapi.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -16,5 +18,13 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		if(user == null) {
+			throw new ObjectNotFoundException("Objeto não encontrado");
+		}
+		return user.get();
 	}
 }
