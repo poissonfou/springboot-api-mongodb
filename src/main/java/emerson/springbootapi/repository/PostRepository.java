6f40d0d6e19ede9708bedd24a320669999cd93e6
Emerson Lima <emersonlimago123@gmail.com>
2024-07-22 21:55:53 -0300

    Created Post

--- src/main/java/emerson/springbootapi/repository/PostRepository.java
+++ src/main/java/emerson/springbootapi/repository/PostRepository.java
@@ -3,9 +3,9 @@ package emerson.springbootapi.repository;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import emerson.springbootapi.domain.User;
+import emerson.springbootapi.domain.Post;
 
 @Repository
-public interface UserRepository extends MongoRepository<User, String> {
+public interface PostRepository extends MongoRepository<Post, String> {
 	
 }
