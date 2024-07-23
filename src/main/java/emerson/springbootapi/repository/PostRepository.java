845aa3c4eccb7ebbe7ea878dace4e2a5800ed14d
Emerson Lima <emersonlimago123@gmail.com>
2024-07-23 18:22:35 -0300

    Added post search with @Query

--- src/main/java/emerson/springbootapi/repository/PostRepository.java
+++ src/main/java/emerson/springbootapi/repository/PostRepository.java
@@ -3,12 +3,16 @@ package emerson.springbootapi.repository;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import emerson.springbootapi.domain.Post;
 
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
+	
+	@Query("{ 'title': {$regex: ?0, $options: 'i'} }")
+	List<Post>searchTitle(String text);
 	
 	//this is a native method to Springboot and by just delaring it like this 
 	//it can be used
