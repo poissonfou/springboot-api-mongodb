44ebb36c6b487bd28d4413c8247c57cddcb0849d
Emerson Lima <emersonlimago123@gmail.com>
2024-07-23 15:05:34 -0300

    Added endpoint to get post by id

---
+++ src/main/java/emerson/springbootapi/services/PostService.java
@@ -0,0 +1,23 @@
+package emerson.springbootapi.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import emerson.springbootapi.domain.Post;
+import emerson.springbootapi.domain.User;
+import emerson.springbootapi.repository.PostRepository;
+import emerson.springbootapi.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;
+	
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+		return post.orElseThrow(() -> new  ObjectNotFoundException("Objeto não encontrado"));
+	}
+}
