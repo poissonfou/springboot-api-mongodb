d2d4ddf34e72826e8b9776086c4703fbb2ebfaef
Emerson Lima <emersonlimago123@gmail.com>
2024-07-23 15:00:00 -0300

    Added endpoint to get posts from a user

--- src/main/java/emerson/springbootapi/resources/UserResource.java
+++ src/main/java/emerson/springbootapi/resources/UserResource.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import emerson.springbootapi.domain.Post;
 import emerson.springbootapi.domain.User;
 import emerson.springbootapi.dto.UserDTO;
 import emerson.springbootapi.services.UserService;
@@ -61,6 +62,12 @@ public class UserResource {
 		obj.setId(id);
 		obj = service.update(obj);
 		return ResponseEntity.noContent().build();
+	}
+	
+	@GetMapping(value = "/{id}/posts")
+	public ResponseEntity<List<Post>> findPosts(@PathVariable String id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj.getPosts());
 	}
 	
 }
