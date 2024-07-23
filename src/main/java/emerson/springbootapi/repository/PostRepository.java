4a998e23d214ea72e5810ca7fcb232d9f4e9d4ce
Emerson Lima <emersonlimago123@gmail.com>
2024-07-23 18:46:28 -0300

    Added multiple  query search

--- src/main/java/emerson/springbootapi/repository/PostRepository.java
+++ src/main/java/emerson/springbootapi/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package emerson.springbootapi.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -17,4 +18,7 @@ public interface PostRepository extends MongoRepository<Post, String> {
 	//this is a native method to Springboot and by just delaring it like this 
 	//it can be used
 	List<Post> findByTitleContainingIgnoreCase(String text);
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: {$lte: ?2} }, { $or: [ { 'title': {$regex: ?0, $options: 'i'} }, {  'body': {$regex: ?0, $options: 'i'} }, { 'comments.text': {$regex: ?0, $options: 'i'} } ] } ] }")
+	List<Post> fullSearch(String text, Date minDate, Date maxDate);
 }
--- src/main/java/emerson/springbootapi/resources/util/URL.java
+++ src/main/java/emerson/springbootapi/resources/util/URL.java
@@ -2,14 +2,30 @@ package emerson.springbootapi.resources.util;
 
 import java.io.UnsupportedEncodingException;
 import java.net.URLDecoder;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.TimeZone;
+
+import org.springframework.expression.ParseException;
 
 public class URL {
-	
+
 	public static String decodeParam(String text) {
 		try {
 			return URLDecoder.decode(text, "UTF-8");
 		} catch (UnsupportedEncodingException e) {
 			return "";
 		}
+	}
+
+	public static Date convertDate(String textDate, Date defaultValue) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		sdf.setTimeZone(TimeZone.getTimeZone("GMT"));
+		try {
+			return sdf.parse(textDate);
+		} catch (java.text.ParseException e) {
+			return defaultValue;
+		}
+
 	}
 }
