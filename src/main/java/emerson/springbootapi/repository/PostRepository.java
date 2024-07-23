74c7bbf5e62a38cafe589dbac15abba4f968f706
Emerson Lima <emersonlimago123@gmail.com>
2024-07-23 18:16:15 -0300

    Added simple post search with query methods

--- src/main/java/emerson/springbootapi/repository/PostRepository.java
+++ src/main/java/emerson/springbootapi/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package emerson.springbootapi.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,7 @@ import emerson.springbootapi.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String> {
 	
+	//this is a native method to Springboot and by just delaring it like this 
+	//it can be used
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/emerson/springbootapi/services/PostService.java
+++ src/main/java/emerson/springbootapi/services/PostService.java
@@ -1,12 +1,12 @@
 package emerson.springbootapi.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import emerson.springbootapi.domain.Post;
-import emerson.springbootapi.domain.User;
 import emerson.springbootapi.repository.PostRepository;
 import emerson.springbootapi.services.exception.ObjectNotFoundException;
 
@@ -19,5 +19,9 @@ public class PostService {
 	public Post findById(String id) {
 		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(() -> new  ObjectNotFoundException("Objeto não encontrado"));
+	} 
+	
+	public List<Post> findByTitle(String text){
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 }
